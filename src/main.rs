87ec978ce8c299b1c011290bdabd4f0be5f79175
dbgd <bberghie@gmail.com>
2025-07-24 23:28:26 +0200

    ok it works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::env;
-use dotenv::dotenv;
+use dotenvy::dotenv;
+
+#[derive(Debug)]
+pub struct Config{
+    pub discord_token: String,
+    pub machine_mac: String,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, Box<dyn std::error::Error>> {
+        // Load the .env file
+        dotenv().ok();
+
+        // Leer variables (con unwrap_or para dar errores claros)
+        let discord_token = env::var("DISCORD_TOKEN")?;
+        let machine_mac = env::var("MACHINE_MAC")?;
+
+        Ok(Self {
+            discord_token,
+            machine_mac,
+        })
+    }
+}
 
 fn main() {
-    dotenv().ok();
-    match env::var("MACHINE_MAC") {
-        Ok(machine_mac) => println!("the machine's mac is {}", machine_mac),
-        Err(e) => println!("Could't read machine_mac ({})", e),
-    };
+    match Config::from_env() {
+       Ok(config) => println!("{:#?}", config),
+       Err(error) => panic!("{:#?}", error)
+   }
 }
