722a13fe94e397b8abb3fea3c009dd8987193c06
Bogdan Berghie <bogdan.berghie@kaytek.es>
2025-07-24 16:42:56 +0200

    reding from env.... win?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+use std::path::{Path};
+
 fn main() {
     println!("test");
+
+    let my_path = env::home_dir().and_then(|a| Some(a.join("/.env"))).unwrap();
+    dotenv::from_path(my_path.as_path());
+
+    for (k, v) in env::vars() {
+        println!("{}: {}", k, v);
+    }
 }
